1aba2ab54a9367b33f1b54982ceeea4fb47a9288
Ankur <47654709+ankur4692@users.noreply.github.com>
2022-03-25 21:22:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,5 @@
-variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "us-east-2"
-}
-
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
-}
-
-variable "instance_type" {
-  description = "instance type for ec2"
-  default     =  "t2.micro"
-}
-
-variable "security_group" {
-  description = "Name of security group"
-  default     = "my-jenkins-security-group"
-}
-
-variable "tag_name" {
-  description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
-}
-
-variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIASNPFKRHV5FMDX6DA"
+  secret_key = "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"
 }
